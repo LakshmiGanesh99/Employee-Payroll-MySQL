4eaf9c2a39e3c965a3fcd6b45c346bc016e475d1
LakshmiGanesh <71899781+LakshmiGanesh99@users.noreply.github.com>
2020-11-02 00:43:15 +0530

    [LakshmiGanesh] create: employee_payroll table created

--- payroll_service.sql
+++ payroll_service.sql
@@ -2,3 +2,12 @@
 CREATE DATABASE payroll_service;
 show databases;
 USE payroll_service; 
+
+# UC2 creating table
+CREATE TABLE employee_payroll
+(
+id INT UNSIGNED NOT NULL auto_increment, name VARCHAR(150) NOT NULL,
+salary DOUBLE NOT NULL,
+start DATE NOT NULL,
+PRIMARY KEY (id)
+);
